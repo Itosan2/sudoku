372028480da03c703809ff5201f964310557d4a0
YT Choong <e2choong@yahoo.com>
2023-05-11 21:58:40 +0800

    added new features

--- static/js/index.js
+++ static/js/index.js
@@ -1,18 +1,28 @@
 const container = document.querySelector(".container");
-const winWidth = document.querySelector("#win-width");
-const conWidth = document.querySelector("#con-width");
+// const winWidth = document.querySelector("#win-width");
+// const conWidth = document.querySelector("#con-width");
+const hbBtn = document.querySelector(".hamburger-btn");
+const mainGameInfo = document.querySelector(".main-game-info");
 
-window.addEventListener("resize", () => {
-  const windowWidth = container.clientWidth;
-
-  winWidth.textContent = `Win: ${windowWidth}px`;
-});
-
-const observer = new ResizeObserver((entries) => {
-  for (let entry of entries) {
-    const width = Math.floor(entry.contentRect.width);
-    conWidth.textContent = ` Con:${width}px`;
+hbBtn.addEventListener("change", (e) => {
+  if (e.target.checked) {
+    mainGameInfo.classList.toggle("active");
+  } else {
+    mainGameInfo.classList.toggle("active");
   }
 });
 
-observer.observe(container);
+// window.addEventListener("resize", () => {
+//   const windowWidth = container.clientWidth;
+
+//   winWidth.textContent = `Win: ${windowWidth}px`;
+// });
+
+// const observer = new ResizeObserver((entries) => {
+//   for (let entry of entries) {
+//     const width = Math.floor(entry.contentRect.width);
+//     conWidth.textContent = ` Con:${width}px`;
+//   }
+// });
+
+// observer.observe(container);
